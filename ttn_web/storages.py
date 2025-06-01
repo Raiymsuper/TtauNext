a15a2716cc8ee0dafa5394f0c6acace45c3b512e
Raiymbek K <raiymbek200516@gmail.com>
2025-06-02 03:33:52 +0500

    Docker added

--- ttn_web/storages.py
+++
@@ -1,7 +0,0 @@
-from minio_storage.storage import MediaCloudStorage
-
-class VideoStorage(MediaCloudStorage):
-    bucket_name = "videos"
-
-class LogoStorage(MediaCloudStorage):
-    bucket_name = "logos"

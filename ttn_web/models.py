4376490949b87806d18bb32f5cac4dd3aba50881
Raiymbek K <raiymbek200516@gmail.com>
2025-06-09 16:51:10 +0500

    lol3

--- ttn_web/models.py
+++ ttn_web/models.py
@@ -27,7 +27,6 @@ class Video(models.Model):
 class Client(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=60)
-    logo = models.ImageField(upload_to='logos/')
     videos = models.ManyToManyField(Video, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
